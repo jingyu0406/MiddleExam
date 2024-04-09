226c52db79ff67d0e7b4d1be813a778855acb18e
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2024-04-09 21:02:34 +0800

    取代switch改用pressable切換darkmode

--- test/SettingsScreen.js
+++ test/SettingsScreen.js
@@ -8,22 +8,32 @@ import {
     Button,
     ButtonText,
     Text,
+    Pressable
 } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 import MapView, { Marker } from 'react-native-maps';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const SettingsScreen = () => {
     const [counter, setCounter] = useState(0);
-    const [colorMode, setColorMode] = useState("light");
-    const toggleColorMode = () => {
-        if (colorMode == "light") setColorMode("dark");
-        else setColorMode("light");
+    // const [colorMode, setColorMode] = useState("light");
+    // const toggleColorMode = () => {
+    //     if (colorMode == "light") setColorMode("dark");
+    //     else setColorMode("light");
+    // };
+
+    const [toggle, setToggle] = useState("light");
+    const toggleFunction = () => {
+        if (toggle == "light") setToggle("dark")
+        else setToggle("light")
     };
+
+
     const [region, setRegion] = useState({
         longitude: 121.544637,
         latitude: 25.024624,
-        longitudeDelta: 0.001,
-        latitudeDelta: 0.002,
+        longitudeDelta: 0.0001,
+        latitudeDelta: 0.0002,
     })
     const [marker, setMarker] = useState({
         coord: {
@@ -38,7 +48,7 @@ const SettingsScreen = () => {
         <Box flex={1}>
 
             <GluestackUIProvider config={config}>
-                <Center flex={1} bg={colorMode == "light" ? "white" : "black"}>
+                <Center flex={1} bg={toggle == "light" ? "white" : "black"}>
                     <HStack space="3xl">
                         <Button
                             onPress={() => setCounter(counter + 2)}
@@ -61,21 +71,34 @@ const SettingsScreen = () => {
                             </ButtonText>
                         </Button>
                     </HStack>
-                    <Text size="3xl" mt="$10" color={colorMode == "dark" ? "white" : "black"}>
+                    <Text size="3xl" mt="$10" color={toggle == "dark" ? "white" : "black"}>
                         {counter}
                     </Text>
 
                     <HStack mt={20} space={8} alignItems="center">
-                        <Text size="lg" px="$2" color={colorMode == "dark" ? "white" : "black"}>
-                            {colorMode == "light" ? "Light Mode" : "Dark Mode"}
+                        <Text size="lg" px="$2" color={toggle == "dark" ? "white" : "black"}>
+                            {/* {colorMode == "light" ? "Light Mode" : "Dark Mode"} */}
+                            {toggle == "light" ? "Light Mode" : "Dark Mode"}
                         </Text>
-                        <Switch
+                        <Pressable onPress={() => toggleFunction()}>
+                            <Text>{toggle === "light" ? <MaterialCommunityIcons
+                                name={'menu'}
+                                size={20}
+                                color={"black"}
+
+                            /> : <MaterialCommunityIcons
+                                name={'bookmark'}
+                                size={20}
+                                color={"white"}
+                            />}</Text>
+                        </Pressable>
+                        {/* <Switch
                             name="light Mode"
                             value={colorMode === "light"}
                             onToggle={toggleColorMode}
                             accessibilityLabel="display-mode"
                             accessibilityHint="light or dark mode"
-                        />
+                        /> */}
                     </HStack>
 
                 </Center>
@@ -84,8 +107,8 @@ const SettingsScreen = () => {
                     region={region}
                     style={{ flex: 1 }}
                     showsTraffic
-                    mapType='hybrid'>
-                    {/* //standard */}
+                    mapType='Retro'>
+                    {/* //hybrid */}
                     <Marker
                         coordinate={marker.coord}
                         title={marker.name}
