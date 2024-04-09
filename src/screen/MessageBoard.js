9082eaacdb2caadfc399d8f31fac9e040adcd453
jingyu0406 <131228413+jingyu0406@users.noreply.github.com>
2024-04-09 22:01:30 +0800

    0409 FAB
    做了加留言的浮動按鈕

--- src/screen/MessageBoard.js
+++ src/screen/MessageBoard.js
@@ -1,10 +1,22 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, FlatList } from 'react-native';
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
 import Message from '../component/Message';
 import MessageScreen from './MessageScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { useSelector } from 'react-redux';
 import { selectMessage } from '../redux/messageSlice';
+import { useNavigation } from '@react-navigation/native';
+
+import { Pressable, StyleSheet } from 'react-native';
+
+const FloatingActionButton = ({ onPress }) => {
+  return (
+    <Pressable style={styles.FAB} onPress={onPress}>
+      <MaterialCommunityIcons name="plus" size={30} color="white" />
+    </Pressable>
+  );
+};
 
 const MessageBoard = ({navigation}) => {
   const message = useSelector(selectMessage);
@@ -14,7 +26,8 @@ const MessageBoard = ({navigation}) => {
     <View style={{ flex: 1}}>
  
       <Button title="Add" onPress={() => navigation.navigate('留言')} />
-
+      <FloatingActionButton onPress={() => {console.log('Button pressed');
+      navigation.navigate('留言');}} />
       <FlatList
         data={message}
         renderItem={({ item }) => 
@@ -29,5 +42,21 @@ const MessageBoard = ({navigation}) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  FAB: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 60,
+    height: 60,
+    backgroundColor: 'blue',
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex:1000
+  },
+});
+
 
 export default MessageBoard;
