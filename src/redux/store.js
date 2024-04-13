88b29adae48604e5aadd662d481f57ebd30a97a5
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2024-04-14 01:18:59 +0800

    0414 將darkMode修改成全域變數

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,17 @@
-import  {configureStore} from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import messageSlice from './messageSlice'
+import counterReducer from './counterSlice';
 
 export default configureStore({
-    reducer:{
-        message: messageSlice
-    }
+    reducer: {
+        message: messageSlice,
+        counter: counterReducer
+    },
+    devTools: process.env.NODE_ENV !== 'production',
 })
+
+
+
+
+
+
